9ee193a32e9c41f5183c01714e3f2a96066f9d4c
sui_h <78638687+He-Sui@users.noreply.github.com>
2022-04-05 10:29:39 +0800

    Update debounce.v

--- debounce.v
+++ debounce.v
@@ -1,53 +1,15 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 2021/12/16 15:35:01
-// Design Name: 
-// Module Name: debounce
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
-module debounce(
+module deubonce (
 input clk, rst, ori_signal, output pos_signal
 );
 reg[2:0] delay;
-parameter period = 200000;
-reg [31:0] cnt;
-reg clkout;
-always @(posedge clk, posedge rst) begin
-if(rst)
-begin
-    clkout <= 0;
-    cnt <= 0;
-end
-else 
-if(cnt == (period >> 1) - 1)
-begin
-    clkout <= ~clkout;
-    cnt <= 0;
-end
 
-else
-    cnt <= cnt + 1;
-end
-
-always @ ( posedge clkout or posedge rst)
+always @ ( posedge clk or posedge rst)
 if(rst)
     delay <= 0;
 else
-    delay <= {delay[1:0], ori_signal} ;
+    delay <= { delay[1:0], ori_signal} ;
+
 assign pos_signal = delay[1] && ( ~delay[2] );
+
 endmodule
