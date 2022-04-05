00d7a6062803406e11fe206f6e77cd079e129fd1
sui_h <78638687+He-Sui@users.noreply.github.com>
2022-04-05 10:37:25 +0800

    Update encoder_display.v

--- encoder_display.v
+++ encoder_display.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 2021/12/20 16:28:18
-// Design Name: 
-// Module Name: encoder_display
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module encoder_display(
     input clk,
